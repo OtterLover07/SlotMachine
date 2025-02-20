bd84d6bfb910c8c9335f8e060a484f4a211eb686
OtterLover07 <melker@willforss.se>
2025-02-20 14:03:18 +0100

    Create Slot_Machine.ino

---
+++ Slot_Machine.ino
@@ -0,0 +1,26 @@
+////////////////////////////////////////////////////////////////MAKE A SLOT MACHINE////////////////////////////////////////////////////////////////
+/*
+* Name: Slot Machine
+* Author: Melker Willforss
+* Date: 2025-XX-XX
+* Description: TBA
+*/
+
+// Include Libraries
+
+// Init constants
+
+// Init global variables
+
+// construct objects
+
+void setup() {
+  // init communication
+
+  // Init Hardware
+
+}
+
+void loop() {
+
+}
