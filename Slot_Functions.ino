01645315bee55e9647bc30d81bb89dfb89d5e838
OtterLover07 <melker@willforss.se>
2025-04-24 16:05:12 +0200

    Final touches, a long-needed fix
    * Fixed the coin sensor so that it is actually calibrated at the beginning, instead of relying on a flimsy process as it was before.
    * Added comments to all the functions, as well as finally adding that project description.
    * Added sound to the lever to tell the user when it's been pulled down enough. This because of a previous accident where a user didn't know when the lever had hit the button. This caused the lever to break.

--- Slot_Functions.ino
+++ Slot_Functions.ino
@@ -1,3 +1,8 @@
+/*
+* This function dispenses an amount of coins from the coin mechanism.
+* Parameters: x - The amount of coins that sould be dispensed (int)
+* Returns: Void
+*/
 void payout(int x) {
   for (int i = 0; i < x; i++) {
     digitalWrite(2, HIGH);
@@ -7,6 +12,11 @@ void payout(int x) {
   }
 }
 
+/*
+* This function rolls three random symbols from [symbols] and displays them on the screen. This is done an amount of times.
+* Parameters: x - The amount of times the function should be executed (int)
+* Returns: Void
+*/
 void displayRandomSymbols(int x) {
   tft.setTextSize(15);
   for (int i = 0; i < x; i++) {
@@ -17,6 +27,11 @@ void displayRandomSymbols(int x) {
   }
 }
 
+/*
+* This function writes how many credits the user has to the TFT display.
+* Parameters: Void
+* Returns: Void
+*/
 void displayCredits() {
   tft.setTextSize(3);
   tft.setCursor(150, 250);
@@ -25,6 +40,13 @@ void displayCredits() {
   tft.print("        ");
 }
 
+/*
+* This function is responsible for the gambling process. First it randomly shows a few symbols to tease the user.
+* It then rolls three random symbols and stores their values along with displaying them to the user. It then checks
+* the rolled symbols. If the rolled set of symbols is eligible for a payout, the corresponding payout is then given.
+* Parameters: Void
+* Returns: Void
+*/
 void gamble() {
   displayRandomSymbols(30);
   int rolled[3] = {};
@@ -34,6 +56,7 @@ void gamble() {
     tft.write(rolled[i]);
   }
 
+  // Check if the rolled symbols should give a payout, and do if so.
   if (rolled[0] == rolled[1] && rolled[0] == rolled[2] && rolled[0] == 0x0F) {
     payout(10);
     Serial.println("Jackpot! (15)");
@@ -55,17 +78,24 @@ void gamble() {
   }
 }
 
+/*
+* This function checks the coin sensor. If a coin has been inserted it then increments the credits variable by 1.
+* Parameters: Void
+* Returns: Void
+*/
 void checkCredits() {
   currentReading[0] = analogRead(A5);
   delay(5);
   currentReading[1] = analogRead(A5);
   int reading = (currentReading[0] + currentReading[1])/2;
-  //Serial.println(reading);
-  if (reading < (lastReading - sensitivity)) {
+  if (reading < (lastReading - 5) || reading > (lastReading + 5)) {
+    Serial.println(reading);
+  }
+  if (reading > (ambientLight + sensitivity)) {
     delay(200);
     credits += 1;
     Serial.println("Credit inserted!");
     displayCredits();
   }
   lastReading = reading;
-}
+  }
--- Slot_Machine.ino
+++ Slot_Machine.ino
@@ -3,7 +3,12 @@
 * Name: Slot Machine
 * Author: Melker Willforss
 * Date: 2025-03-06
-* Description: TBA
+* Description:
+* This project waits for a token to trigger the sensor at A5, increasing credits. When the user has aquired credits,
+* they can then pull a lever wich activates pin 3. This rolls three symbols and checks if the layout is valid for
+* any payouts. If so, a corresponding amount of tokens is dispensed using pin 2. Every "spin" also costs 1 credit.
+* In addition, the project plays sound through a speaker on pin 6 every time the lever is pulled, in order to help
+* the user tell when it's been pulled far enough.
 */
 
 // Include Libraries
@@ -12,7 +17,7 @@
 #include <SPI.h>
 
 // Init constants
-const int sensitivity = 80;
+const int sensitivity = 50;
 const int knapp = 3;
 const int dispenser = 2;
 const int symbols[11] = {0x03,0x03,0x04,0x04,0x05,0x05,0x06,0x06,0x0E,0x0E,0x0F};
@@ -21,8 +26,7 @@ const int symbols[11] = {0x03,0x03,0x04,0x04,0x05,0x05,0x06,0x06,0x0E,0x0E,0x0F}
 int credits = 0;
 int currentReading[2] = {0,0};
 int lastReading = 0;
-
-// construct objects
+int ambientLight = 0;
 
 //Setup TFT
 typedef Xilka::HX8357D DisplayType;
@@ -33,7 +37,7 @@ const int8_t PIN_DC(9);
 DisplayType tft(PIN_RESET, PIN_DC, PIN_CS);
 
 void setup() {
-  // init communication
+  // init Communication
   Serial.begin(9600);
   spiBegin();
 
@@ -41,6 +45,18 @@ void setup() {
   pinMode(dispenser, OUTPUT); 
   pinMode(3, INPUT_PULLUP);
   randomSeed(analogRead(A0));
+  
+  // Calibrate Coin Sensor
+  int lightSetup = 0;
+  for (int i = 0; i < 10; i++) {
+    lightSetup += analogRead(A5);
+    delay(5);
+  }
+  ambientLight = lightSetup/10;
+  Serial.println(ambientLight + sensitivity);
+  delay(600);
+
+  // Setup Screen and Final Touches
   tft.begin();
   tft.fillScreen(tft.BLACK);
   tft.setTextColor(tft.WHITE,tft.BLACK);
@@ -59,10 +75,15 @@ void loop() {
   while (digitalRead(knapp) == HIGH) {
     checkCredits();
   }
-  if (credits > 0) {
+  tone(6, 500, 200); //plays sound to warn the user that the lever's been pulled all the way
+  while (digitalRead(knapp) != HIGH) {
+    if (credits > 0) {
     credits -= 1;
     displayCredits();
     gamble();
+    } else {
+      tone(6, 500); //keep playing the sound if no credits are logged
+    }
   }
-  
+  noTone(6); 
 }
